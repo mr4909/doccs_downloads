817b220f7d30c522752c09e67ad7274eb1f8d8e1
Evan Misshula <emisshula@jjay.cuny.edu>
2020-09-30 10:34:43 -0400

    changes format to download pdf

--- schedule_script_addin.R
+++ schedule_script_addin.R
@@ -11,7 +11,7 @@ p_load('miniUI')
 p_load('shiny')
 p_load('shinyFiles')
 
-# current_time <- Sys.time()
+current_time <- Sys.time()
 # print(current_time)
 # msg <- glue::glue("This is a test I'm running at {current_time}.")
 # cat(msg, file = "test.text")
